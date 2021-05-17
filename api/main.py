5eec9f205bead018bd52ceb037befd1b33e75120
kremrik <kyle.robert.e@gmail.com>
2021-05-16 21:25:38 -0500

    addition of init file fixed mypy screaming

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
-from api.routes.auth import router as auth  # type: ignore
-from api.routes.base import router as base  # type: ignore
-from api.routes.passages import router as passages  # type: ignore
-from api.routes.users import router as users  # type: ignore
+from api.routes.auth import router as auth
+from api.routes.base import router as base
+from api.routes.passages import router as passages
+from api.routes.users import router as users
 from api.middleware.cors import CORS
 from api.db.engine import engine
 
